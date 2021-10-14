ad1e819a461a7ad8cb9bedd7844450d9d8231e36
이정은 <wwwwwet96@gmail.com>
2021-10-15 01:04:44 +0900

    완주하지 못한 선수

--- hash/hash1.py
+++ hash/hash1.py
@@ -3,7 +3,7 @@ import collections
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
 
-    return list(answer.keys())
+    return list(answer.keys())[0]
     '''문제에서 단 한명의 선수만 완주하지못했다고 했으니까 [0]붙인 것
     [0]제외하고 제출하면 시간 에러 나는듯'''
 solution(["leo", "kiki", "eden","lee"], ["eden", "kiki"])

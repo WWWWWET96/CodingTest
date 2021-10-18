f6b723ba1366f3df0a85c86922a118fafdeddd0d
이정은 <wwwwwet96@gmail.com>
2021-10-18 22:35:24 +0900

    entirety1:모의고사

---
+++ entirety/entirety1.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    result = []
+
+    a = [1,2,3,4,5]
+    b = [2,1,2,3,2,4,2,5]
+    c = [3,3,1,1,2,2,4,4,5,5]
+    score = [0,0,0]
+    for idx, answer in enumerate(answers):# idx와 answer같이
+        if answer == a[idx % len(a)]:
+            score[0] += 1
+        if answer == b[idx % len(b)]:
+            score[1] += 1
+        if answer == c[idx % len(c)]:
+            score[2] += 1
+
+    for idx,s in enumerate(score):
+        if s == max(score): result.append(idx+1)
+
+    return result
+
+
+print(solution([1,3,2,4,2]))
